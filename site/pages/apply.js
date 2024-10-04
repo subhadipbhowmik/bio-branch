a8416d6e825e5516030824d85a1a4e6ada334a39
manu-hn <manuu.hn11@gmail.com>
2024-10-04 16:00:46 +0530

    Implemented Toggle password visibility with eye icon for both Login and Register Page

--- site/pages/apply.js
+++ site/pages/apply.js
@@ -3,7 +3,7 @@ import styles from '../styles/apply.module.css';
 import { toast } from 'react-toastify';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
+import { Eye, EyeOff, Mail } from 'lucide-react';
 const Apply = () => {
   const router = useRouter();
   const [handle, setHandle] = useState('');
@@ -11,6 +11,8 @@ const Apply = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [submitted, setSubmitted] = useState(false);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [passwordType, setPasswordType] = useState('');
 
   const handleCategoryChange = (e) => {
     setCategory(e.target.value);
@@ -59,38 +61,59 @@ const Apply = () => {
             <form onSubmit={handleRegister} className='flex flex-col gap-3 text-lg' method='POST'>
               <span className='flex items-center shadow-md border-2 px-3 py-2 rounded-md focus:outline-none'>
                 <img className='w-6 mr-2' src="/svg/instagram.svg" alt="" />
-                <input 
-                  className='focus:outline-none' 
-                  placeholder='Social Handle' 
-                  type="text" 
-                  value={handle} 
-                  onChange={(e) => setHandle(e.target.value)} 
+                <input
+                  className='focus:outline-none'
+                  placeholder='Social Handle'
+                  type="text"
+                  value={handle}
+                  onChange={(e) => setHandle(e.target.value)}
                 />
               </span>
-              <input 
-                className='shadow-md border-2 px-3 py-2 rounded-md focus:outline-none' 
-                placeholder='Add Email' 
-                type="email" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)} 
-              />
-              <input 
-                className='shadow-md border-2 px-3 py-2 rounded-md focus:outline-none' 
-                placeholder='Add Password' 
-                type="password" 
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)} 
+
+              <span className='shadow-md border-2 px-3 py-2 rounded-md  flex items-center'>
+                <Mail />
+                <input
+                  className='px-3 rounded-md focus:outline-none'
+                  placeholder='Add Email'
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </span>
+              {/* <input
+                className='shadow-md border-2 px-3 py-2 rounded-md focus:outline-none'
+                placeholder='Add Password'
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 required
-              />
+              /> */}
+              <span className='shadow-md border-2 px-3 py-2 rounded-md focus:outline-none flex items-center'>
+                {
+                  isPasswordVisible ? (
+                    <EyeOff onClick={() => setIsPasswordVisible(false)} className="cursor-pointer" />
+                  ) : (
+                    <Eye onClick={() => setIsPasswordVisible(true)} className="cursor-pointer" />
+                  )
+                }
+                <input
+                  className="px-3 focus:ring-0 border-none focus:outline-none ml-2"
+                  placeholder='Add Password'
+                  type={isPasswordVisible ? "text" : "password"}
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </span>
 
               <h5 className='text-center text-sm text-blue-600'>Account Type</h5>
               <span className="flex justify-between">
                 <label htmlFor="" className='flex gap-2'>
-                  <input type="radio" value='Creator' checked={category === 'Creator'} onChange={handleCategoryChange}/>
+                  <input type="radio" value='Creator' checked={category === 'Creator'} onChange={handleCategoryChange} />
                   <p>Creator</p>
                 </label>
                 <label htmlFor="" className='flex gap-2'>
-                  <input type="radio" value='Agency' checked={category === 'Agency'} onChange={handleCategoryChange}/>
+                  <input type="radio" value='Agency' checked={category === 'Agency'} onChange={handleCategoryChange} />
                   <p>Agency</p>
                 </label>
                 <label htmlFor="" className='flex gap-2'>
@@ -99,7 +122,7 @@ const Apply = () => {
                 </label>
               </span>
 
-              <input type='submit' value='Apply' className='bg-blue-500 text-white px-3 py-2 rounded-md'/>
+              <input type='submit' value='Apply' className='bg-blue-500 text-white px-3 py-2 rounded-md' />
             </form>
 
             <h4 className='text-center pt-3'>Already Have Account? <span className='text-indigo-600'><Link href="/login">Login</Link></span></h4>
