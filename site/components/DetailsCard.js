8baa659d2c9ccfea465bb26233a5329b2703ae2f
JyothiPriya5 <jyothipriyaranga0105@gmail.com>
2024-10-07 21:23:21 +0530

    Change in UI of Features

--- site/components/DetailsCard.js
+++ site/components/DetailsCard.js
@@ -5,7 +5,7 @@ const DetailsCard = () => {
   return (
     <section className="py-12">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-semibold text-left mb-4">Features</h2>
+        <h2 className="text-3xl font-semibold text-center mb-4">Features</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           
           {/* Card 1 */}
