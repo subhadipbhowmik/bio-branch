efd5b3d7bd0d7c86e458a217ba3a09405822439e
Sohel Sheikh <sohelxsheikh@gmail.com>
2024-10-08 09:40:36 +0530

    Bcrypt Added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const UserModel = require('../models/user');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
 
 const registerUser = async (req, res) => {
     const { handle, email, password, category } = req.body;
@@ -12,12 +13,16 @@ const registerUser = async (req, res) => {
             icon: 'https://i.ibb.co/Xy5kkZM/link.png'
         };
 
-        const user = await UserModel.create({
-            handle, email, password, role: category, links: [defaultLink]
+      await bcrypt.hash(password, 10, async function(err, hash) {
+             
+            const user = await UserModel.create({
+                handle, email, hash, role: category, links: [defaultLink]
+            });
+        
+            const token = jwt.sign({ email: email }, process.env.SECRET_JWT);
+        
         });
-    
-        const token = jwt.sign({ email: email }, process.env.SECRET_JWT);
-    
+
         return res.json({
             message: 'User Created',
             status: 'success',
@@ -41,7 +46,8 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const user = await UserModel.findOne({ email: email, password: password });
+
+        const user = await UserModel.findOne({ email: email });
         if (!user) {
             return res.json({
                 status: 'notfound',
@@ -49,6 +55,9 @@ const loginUser = async (req, res) => {
             });
         }
 
+        const result = await bcrypt.compare(password, user.password)
+
+      if(result){
         const token = jwt.sign({ email: email }, process.env.SECRET_JWT);
         return res.json({
             message: 'User found',
@@ -56,7 +65,7 @@ const loginUser = async (req, res) => {
             token: token,
             id: user._id
         });
-
+    }
     } catch (error) {
         return res.json({
             message: error.message,
