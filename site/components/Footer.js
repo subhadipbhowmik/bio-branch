c1ea621b8262ac6841a71e0a989f1babb27a2668
Rajdeep Chakraborty <rajdeepchka@gmail.com>
2024-10-15 16:05:56 +0530

    Removed: Poor Wave SVG

--- site/components/Footer.js
+++ site/components/Footer.js
@@ -8,7 +8,10 @@ const Footer = () => {
 
       {/* Wave Effect (You can add SVG wave here for a design touch) */}
       <svg className="absolute top-0 left-0 right-0 -mt-1 h-8" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
-        <path fill="#1a202c" fillOpacity="1" d="M0,224L48,208C96,192,192,160,288,160C384,160,480,192,576,186.7C672,181,768,139,864,133.3C960,128,1056,160,1152,176C1248,192,1344,192,1392,192L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+        <path fill="#1a202c" fillOpacity="1" 
+        // Insert necessary path of SVG wave
+        d="">
+        </path>
       </svg>
 
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between relative z-10 space-y-8 md:space-y-0">
