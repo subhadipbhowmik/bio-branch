1a145de0eab3910ad100378de9630414383a19e1
K N Meghana <meghanakn473@gmail.com>
2024-11-06 22:20:37 +0530

    Add dark mode toggle button

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,20 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Enable dark mode support
+
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {
       colors: {
         gray: {
           900: 'rgb(7, 56, 65)', // Custom color for footer section
         }
-      }
+      },
     },
   },
+
+  variants: {
+    extend: {
+      backgroundColor: ['dark'], // Enable dark mode support for background color
+      textColor: ['dark'], // Enable dark mode support for text color
+    },
+  },
+
   plugins: [],
+
   safelist: [
     'bg-red-500',
     'bg-blue-500',
@@ -22,4 +34,4 @@ module.exports = {
     'bg-yellow-500',
     'bg-purple-500',
   ],
-}
+};
